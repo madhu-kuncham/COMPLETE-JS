5a3efd145c46fc6cedb56be50885eed8e914e5cd
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-06-20 11:15:17 +0530

    add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,18 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = currentResult + 15;
 
-outputResult(currentResult , '');
+function add(num1 , num2){
+    const result = num1 + num2;
+    alert('The result is ' + result)
+}
+
+add(1 ,2);
+add(18 , 97);
+
+currentResult = (currentResult + 10) *3 / 2 -1;
+
+let calculationDescription = `(${defaultResult}  + 10 ) * 3 / 2 - 1`;
+
+outputResult(currentResult , calculationDescription);
+
